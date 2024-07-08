fc6eb54f486a5afb21016ec336ce7aa655ffc41e
Geymerson Câmara <geymersonsuporte@gmail.com>
2024-07-08 08:39:19 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
 
-const offset = 0;
-const limit = 10;
 const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 
 function convertPokemonToLi(pokemon) {
@@ -11,7 +9,7 @@ function convertPokemonToLi(pokemon) {
 
                 <div class="detail">
                     <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                     </ol>
 
                     <img src="${pokemon.photo}"
