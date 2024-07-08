433185b1f276a1e81ece461ae7c63cda03acfdca
Geymerson Câmara <geymersonsuporte@gmail.com>
2024-07-07 23:38:20 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,9 +2,16 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon => {
+    return fetch(pokemon.url).then((response) => response.json())
+})
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
     return fetch(url)
         .then((Response) => Response.json())
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detaiRequests) => Promise.all(detaiRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
